8493eb1daf5b37ff240c71b66bb5c569056553da
iryna-h29 <irenegavrilenko@gmail.com>
2026-05-04 15:53:11 +0200

    Refactor: confirm FSM implementation in setContent utility

--- src/utils/setContent.js
+++ src/utils/setContent.js
@@ -7,16 +7,12 @@ const setContent = (process, Component, data, func) => {
     switch (process) {
         case 'waiting':
             return <Skeleton/>;
-            break;
         case 'loading':
             return <Spinner/>;
-            break;
         case 'confirmed':
             return <Component data={data} func={func}/>;
-            break;
         case 'error':
             return <ErrorMessage/>;
-            break;
         default:
             throw new Error('Unexpected process state');
     }
